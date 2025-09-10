d6ad62d2faf2fa83ab626a05f13807893fd9b017
trabajo12344 <davidgumera.w.m@gmail.com>
2025-09-10 09:09:31 -0300

    database.py

--- App/database.py
+++ App/database.py
@@ -1,1 +1,13 @@
+//database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Datos de conexión a MariaDB
+DATABASE_URL = "mariadb+mariadbconnector://usuario:password@localhost:3306/mi_basedatos"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
